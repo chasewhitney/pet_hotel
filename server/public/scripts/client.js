9c2141e4e30367ad08b3e2c011695bfcf59c84ca
Tom Brossart <tom@blueraven.digital>
2017-07-12 15:43:59 -0500

    initial client.js ssetup

---
+++ server/public/scripts/client.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  addClickHandlers();
+  refreshPets();
+}); // end of document.ready
+
+// route is /pets
+
+function addClickHandlers() {
+
+}
+
+// READ a.k.a. GET a.k.a. SELECT
+function refreshPets() {
+  $.ajax({
+    type: 'GET',
+    url: '/pets',
+    success: function(response) {
+      console.log("GET response", response.pets);
+      appendToDom(response.pets);
+    },
+  });
+} // end of refreshPets
+
+// this function fills in page
+function appendToDom(pets) {
+  // clear current div
+
+  // big for loop that loops through data and appends to correct positions in DOM
+  for(var i = 0; i < pets.length; i+= 1) {
+    var pet = pets[i];
+    var $tr = $('<tr data-petid="' + pet.id + '"></tr>');
+    // $tr.data('pet', pet);
+    $tr.append('<td class="pet ' + pet.status + '">' + pet.pets + '</td>');
+    // if pet is already complete just add Delete button
+    if(pet.status == "complete") {
+      $tr.append('<td><button class= "deleteBtn" data-petid="'+ pet.id +'">Delete</button></td>');
+      $('#petTable').append($tr);
+    }
+    // otherwise add Done and Delete button
+    else {
+      $tr.append('<td><button class="mark ' + pet.status + '">Done</button>' +
+      '<button class= "deleteBtn" data-petid="'+ pet.id +'">Delete</button></td>');
+      //  incomplete pets go to top of table
+      $('#petList').prepend($tr);
+
+
+}
