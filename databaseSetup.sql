b6ee6f1b6f08081b86ee5302a11bda367be57a04
Chase Whitney <ChaseEWhitney@gmail.com>
2017-07-12 16:39:47 -0500

    updated database setup

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -14,7 +14,25 @@ CREATE TABLE pets (
 
 CREATE TABLE visits (
  id SERIAL PRIMARY KEY,
- check_in_date TIMESTAMP DEFAULT TIMESTAMP 'now()',
+ check_in_date date DEFAULT NULL,
  check_out_date date DEFAULT NULL,
  pet_id int REFERENCES pets
 );
+
+--Dummy data setup
+
+INSERT INTO owners
+VALUES (1, 'Bill', 'Jenkins');
+
+INSERT INTO owners
+VALUES (2, 'Susan', 'Boyle');
+
+INSERT INTO pets
+VALUES (1, 'Sparky', 'black lab', 'black', 1), (2, 'Barksy', 'poodle', 'white', 2);
+
+INSERT INTO visits ("pet_id")
+VALUES (1), (2);
+
+SELECT owner_id, pet_id, first_name, last_name, name, breed, color, check_in_date, check_out_date from owners
+JOIN pets on pets.owner_id = owners.id
+JOIN visits on pets.id = visits.pet_id;
