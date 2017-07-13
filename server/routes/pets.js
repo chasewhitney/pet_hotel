992ffe2d36229b814973f16d093bcb0b85b11a15
Emily Kuplic <emilykuplic@gmail.com>
2017-07-13 10:12:18 -0500

    added post routes to pets.js

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -4,6 +4,49 @@ var router = express.Router();
 var poolModule = require('../modules/pool.js');
 var pool = poolModule;
 
+//post for pet owner name inputs
+router.post('/', function(req, res){
+  console.log('in post route', req.body);
+  pool.connect(function(errConnectingToDatabase, db, done){
+    if(errConnectingToDatabase) {
+      console.log('There was an error connecting to the database', errConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      db.query('INSERT INTO tasks (name) VALUES ($1)', [], function(errMakingQuery, result){
+        done();
+        if(errMakingQuery) {
+          console.log('There was an error making the INSERT query', errMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });//end of pool
+});//end of post
+
+//post for pet name, color, breed inputs
+router.post('/', function(req, res){
+  console.log('in post route', req.body);
+  pool.connect(function(errConnectingToDatabase, db, done){
+    if(errConnectingToDatabase) {
+      console.log('There was an error connecting to the database', errConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      db.query('INSERT INTO tasks (name) VALUES ($1)', [], function(errMakingQuery, result){
+        done();
+        if(errMakingQuery) {
+          console.log('There was an error making the INSERT query', errMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });//end of pool
+});//end of post
+
+//get for the pet information --- populates table on DOM
 router.get('/', function(req,res){
   console.log('in get pets route');
     pool.connect(function(errConnectingToDatabase, db, done){
@@ -11,9 +54,7 @@ router.get('/', function(req,res){
         console.log('There was an error connecting to the database', errConnectingToDatabase);
         res.sendStatus(500);
       } else {
-        db.query('SELECT owner_id, pet_id, first_name, last_name, name, breed, color, check_in_date, check_out_date'
-        + 'FROM owners JOIN pets on pets.owner_id = owners.id' +
-        'JOIN visits on pets.id = visits.pet_id;'), function(errMakingQuery, result){
+        db.query('SELECT owner_id, pet_id, first_name, last_name, name, breed, color, check_in_date, check_out_date FROM owners JOIN pets on pets.owner_id = owners.id JOIN visits on pets.id = visits.pet_id;'), function(errMakingQuery, result){
           done();
           if(errMakingQuery) {
             console.log('There was an error making the SELECT query', errMakingQuery);
@@ -24,6 +65,6 @@ router.get('/', function(req,res){
         };
       }
     });
-});
+}); //end of get
 
 module.exports = router;
